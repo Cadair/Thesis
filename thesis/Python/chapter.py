07d37e7435ddf46ba2ae4f434d207a7b090c9964
Stuart Mumford <stuart@mumford.me.uk>
2014-12-05 11:11:05 +0000

    more changes to pythontex etc.

---
+++ thesis/Python/chapter.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  4 18:01:51 2014
+
+@author: Stuart Mumford
+
+A Class that holds Python information on a chapter level.
+"""
+import os
+import sys
+from collections import OrderedDict
+
+import matplotlib.pyplot as plt
+
+from plotting_helpers import fig_str, sub_fig_str, get_pgf_include
+
+class Chapter(object):
+    """
+    A class holding information about different things in this chapter.
+    """
+    
+    def __init__(self, pytex, number, chapter_path):
+        self.pytex = pytex
+        self._number = number
+        self._chapter_path = chapter_path
+        
+        # Add this chapters Python dir to the sys path
+        self.python_dir = os.path.join(self.chapter_path, 'Python')
+        if not os.path.exists(self.python_dir):
+            os.makedirs(self.python_dir)
+        sys.path.append(self.python_dir)
+        
+        self.data_dir = os.path.join(self.chapter_path, 'Data')
+        if not os.path.exists(self.data_dir):
+            os.makedirs(self.data_dir)
+
+        self.fig_dir = os.path.join(self.chapter_path, 'Figs')
+        if not os.path.exists(self.fig_dir):
+            os.makedirs(self.fig_dir)
+        
+        self.fig_count = 0
+        self._figure_registry = OrderedDict()
+    
+    @property
+    def number(self):
+        return self._number
+    
+    @property
+    def chapter_path(self):
+        return self._chapter_path
+    
+    def data_file(self, file_name):
+        """
+        Get the full path of a data file in this chapters data directory,
+        add it to the pytex tracked files.
+        
+        Parameters
+        ----------
+        file_name : str
+            The filename in the data directory
+        """
+        
+        fname = os.path.join(self.data_dir, file_name)
+        self.pytex.add_dependencies(fname)
+        return fname
+    
+    def save_figure(self, ref, fig=None, fname=None):
+        """
+        Save a figure and track it in this chapter
+        """
+        
+        if fig is None:
+            fig = plt.gcf()
+        
+        if fname is None:
+            fname = 'Chapter{}_Figure{}_{}'.format(self.number, self.fig_count,
+                                                   ref)
+    
+        fname = os.path.join(self.fig_dir, fname)
+        fig.savefig(fname)
+        self.pytex.add_created(fname)
+        
+        self._figure_registry[ref] = {'number': self.fig_count, 'fname': fname}
+        self.fig_count += 1
+    
+        return fname
+    
+    def get_figure(self, ref):
+        """
+        Get the filename of a figure that has already been saved
+        
+        Parameters
+        ----------
+        ref : str
+            The Figure reference
+
+        Returns
+        -------
+        fname : str
+            The filename
+        """
+
+        return self._figure_registry[ref]['fname']
+    
+    def include_figure(self, ref, width=r"\columnwidth", **kwargs):
+        """
+        Print a whole figure environment
+        """
+        
+        fname = self.get_figure(ref)
+    
+        default_kwargs = {'placement':'H', 'caption':'Figure {}'.format(ref),
+                          'label':'fig_{}'.format(ref)}
+        default_kwargs.update(kwargs)
+        
+        myfig = get_pgf_include(fname)
+
+        return fig_str.format(myfig=myfig, **default_kwargs)
---
+++ thesis/Python/plotting_helpers.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import os
+import matplotlib.pyplot as plt
+
+__all__ = ['fig_str', 'sub_fig_str', 'get_pgf_include']#, 'get_pgf_include', 'get_fig_env', 'get_subfig_env',
+#           'make_fig_env', 'make_sub_figs']
+#==============================================================================
+# Latex helpers
+#==============================================================================
+# Some LaTeX code snippiets
+fig_str = r"""
+\begin{{figure*}}
+    {myfig}
+    \caption{{{caption}}}
+    \label{{{label}}}
+\end{{figure*}}
+"""
+
+sub_fig_str = r"""
+\begin{{subfigure}}[{placement}]{{{width}}}
+    {myfig}
+    \caption{{{caption}}}
+    \label{{{label}}}
+\end{{subfigure}}"""
+
+def get_pgf_include(fname, width=r"\columnwidth"):
+    return r"\pgfimage[width={1}]{{{0}}}".format(fname, width)
+
+def get_fig_env(figure_str, **kwargs):
+    """
+    Print a whole figure environment
+    """
+    global fig_count, fig_str
+
+    default_kwargs = {'placement':'H', 'caption':'caption',
+                      'label':'fig_{}'.format(fig_count)}
+
+    default_kwargs.update(kwargs)
+
+    return fig_str.format(myfig=figure_str, **default_kwargs)
+
+def get_subfig_env(subfig_str, caption, label, subplacement='b',
+                   subwidth=r'\textwidth'):
+
+    return sub_fig.format(placement=subplacement, width=subwidth,
+                          caption=caption, label=label, myfig=subfig_str)
+
+def make_fig_env(cfg, **kwargs):
+    fname = kwargs.pop('fname', None)
+    fig = kwargs.pop('fig', None)
+    width = kwargs.pop('width', r"\columnwidth")
+
+    fname = save_fig(cfg, fig=fig, fname=fname)
+    fig_str = get_pgf_include(fname, width=width)
+
+    print(get_fig_env(fig_str, **kwargs))
+
+    return fname
+
+def make_sub_figs(cfg, figs, captions, labels=None, subplacement='b', subwidth=r'\textwidth',
+                  subbefore='\n', subafter='\n', **kwargs):
+    global fig_count
+
+    if len(figs) != len(captions) != len(labels):
+        raise ValueError("Please specify the same number of labels as figures")
+
+    sub_figs_str = r""
+    fnames = []
+
+    if labels is None:
+        labels = ['fig_{fig_count}']*len(figs)
+
+    for fig, caption, label in itertools.izip(figs, captions, labels):
+        fname = save_fig(cfg, fig=fig)
+        fnames.append(fname)
+        pgf = get_pgf_include(fname, width=subwidth)
+        label = label.format(fig_count=fig_count)
+        sub_figs_str += subbefore + get_subfig_env(pgf, caption, label,
+                                                   subplacement=subplacement,
+                                                  subwidth=subwidth) + subafter
+
+    print(get_fig_env(sub_figs_str, **kwargs))
+    return fnames
